981aa5c765d972bb84b1dceca1acc4f9eaf22ae0
GK-LAPTOP\Robin Gk <kallrobin92@gmail.com>
2017-03-08 09:38:53 +0100

    commit

--- app/app.controller.js
+++ app/app.controller.js
@@ -1,3 +1,15 @@
-angular.module("app").controller("appController", ["$scope", function($scope) {
+angular.module("app").
+controller("appController", ["$scope", "appService", "appServiceFactory", function($scope, appService, appServiceFactory) {
 
 }]);
+
+
+// angular.module("app").
+// controller("appController", ["$scope", "appService", "appServiceFactory", function($scope, appService, appServiceFactory) {
+//     $scope.adderaNumbers = function () {
+//         $scope.svar = appService.addNumbers($scope.tal1, $scope.tal2)
+//     };
+//     $scope.subNumbers = function () {
+//         $scope.svar = appServiceFactory.subNumbers($scope.tal1, $scope.tal2)
+//     };
+// }]);
